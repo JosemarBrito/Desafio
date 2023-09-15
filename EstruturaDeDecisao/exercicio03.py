c5020d1098db6a39e2f8c62cbed880faf2d58048
Josemar Brito <estudos.josemarbrito@gmail.com>
2023-09-14 23:23:13 -0300

    execicio 03 resolvido

--- EstruturaDeDecisao/exercicio03.py
+++ EstruturaDeDecisao/exercicio03.py
@@ -1,2 +1,17 @@
 #Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 
+"""print (5<6)
+print (5>6)
+print(5==6)"""
+
+sexo = input('Digite seu sexo. (M) Masculino, (F) Feminino, (I) Indefinido :').upper()
+if sexo == 'M':
+    print ('Masculino')
+elif sexo == 'F':
+    print ('Feminino')
+elif sexo == 'I':
+    print('Indefinido')
+else:
+    print ('Sexo Inválido')
+
+print ('Finalizou o Programa')
